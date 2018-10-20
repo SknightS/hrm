8d35f3d6cd44ac6a83e7f84ea962205acf5fe840
sakib <md.sakibrahman@gmail.com>
2018-10-20 17:57:37 +0600

    sakib

--- login.php
+++ login.php
@@ -1,21 +1,7 @@
 <!DOCTYPE html>
 <html>
-    
-<!-- Mirrored from dreamguys.co.in/smarthr/blue/login.php by HTTrack Website Copier/3.x [XR&CO'2014], Fri, 19 Oct 2018 15:26:35 GMT -->
-<head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0">
-		<link rel="shortcut icon" type="image/x-icon" href="assets/img/favicon.png">
-        <title>Login - HRMS admin template</title>
-		<link href="https://fonts.googleapis.com/css?family=Montserrat:300,400,500,600,700" rel="stylesheet">
-        <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css">
-        <link rel="stylesheet" type="text/css" href="assets/css/font-awesome.min.css">
-        <link rel="stylesheet" type="text/css" href="assets/css/style.css">
-		<!--[if lt IE 9]>
-			<script src="assets/js/html5shiv.min.js"></script>
-			<script src="assets/js/respond.min.js"></script>
-		<![endif]-->
-    </head>
+
+<?php include ('head.php') ?>
     <body>
         <div class="main-wrapper">
 			<div class="account-page">
